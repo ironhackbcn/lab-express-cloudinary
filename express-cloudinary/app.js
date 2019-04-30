4cd2fc4cc30c1ee11d32faee21b805eda63535d8
Jesus Cebader <zebader@hotmail.com>
2019-05-01 00:57:11 +0200

    lab done

---
+++ express-cloudinary/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+
+var router = require('./routes/index');
+
+mongoose.connect('mongodb://localhost/cloudinary', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', router);
+
+
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+// NOTE: requires a views/error.ejs template
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
+
+module.exports = app;
