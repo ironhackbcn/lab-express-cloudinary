caab1f320d10dccbbe9cce5ab69142ff4b333582
Gusbe <gusbertolo@gmail.com>
2019-04-30 23:46:07 +0200

    Iteration 3

--- express-cloudinary/routes/posts.js
+++ express-cloudinary/routes/posts.js
@@ -3,9 +3,31 @@ var router = express.Router();
 const Post = require('./../models/post');
 
 
+
+//GET '/posts/new'
+router.get('/new', (req, res, next) => {
+  res.render('posts/new');
+});
+
+
+//POST '/posts'
+router.post('/', (req, res, next) => {
+  const {title, description} = req.body;
+  
+  newPost = new Post({title, description});
+
+  newPost.save()
+    .then( (res) => res.redirect('/posts'))
+    .catch( (res) => res.render('posts/new',{messageError:'There is a problem with the insertion'}));
+  
+});
+
+
+
+
 // GET '/posts'
 router.get('/', (req, res, next) => {
-  console.log("hola");
+
   Post.find()
     .then((posts) => {
       res.render('posts/index', {posts});
