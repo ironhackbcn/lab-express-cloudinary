2b791b5dbe4c79f25202734810185b53427fe83b
Gusbe <gusbertolo@gmail.com>
2019-04-30 22:44:27 +0200

    Iteration 1

---
+++ express-cloudinary/models/post.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  title: String,
+  description: String
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
