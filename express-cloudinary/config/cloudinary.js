634db0589de5529550afd36aa17d03ed7c8f7742
screeeen <miguel@screenimplosion.com>
2019-05-01 10:38:27 +0200

    iteration 5. working images

--- express-cloudinary/config/cloudinary.js
+++ express-cloudinary/config/cloudinary.js
@@ -2,11 +2,18 @@ const multer = require("multer");
 const cloudinary = require("cloudinary");
 const cloudinaryStorage = require("multer-storage-cloudinary");
 
+const Post = require("./../models/post");
+
+
+const CLOUD_NAME = 'screeeen';
+const API_KEY = '819378991263422';
+const API_SECRET = 'mRuRVEH_mhZrvUGGtNeeC-zVJfs';
+
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET
   });
   const storage = cloudinaryStorage({
   cloudinary: cloudinary,
@@ -14,4 +21,8 @@ cloudinary.config({
   allowedFormats: ["jpg", "png"],
   transformation: [{ width: 500, height: 500, crop: "limit" }]
   });
+
   const parser = multer({ storage: storage });
+
+
+  module.exports = parser;
--- express-cloudinary/routes/posts.js
+++ express-cloudinary/routes/posts.js
@@ -1,59 +1,31 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const Post = require('./../models/post');
 
-const multer = require("multer");
-const cloudinary = require("cloudinary");
-const cloudinaryStorage = require("multer-storage-cloudinary");
-
-const Post = require("./../models/post");
-
-
-const CLOUD_NAME = 'screeeen';
-const API_KEY = '819378991263422';
-const API_SECRET = 'mRuRVEH_mhZrvUGGtNeeC-zVJfs';
-
-
-cloudinary.config({
-  cloud_name: CLOUD_NAME,
-  api_key: API_KEY,
-  api_secret: API_SECRET
-  });
-  const storage = cloudinaryStorage({
-  cloudinary: cloudinary,
-  folder: "demo",
-  allowedFormats: ["jpg", "png"],
-  transformation: [{ width: 500, height: 500, crop: "limit" }]
-  });
-
-  const parser = multer({ storage: storage });
-
+const parser = require('./../config/cloudinary');
 
 //GET '/posts'
-router.get('/', (req,res,next)=>{
-Post.find()
-.then(allThePosts=> res.render('posts/index',{allThePosts}))
-.catch(err => console.log(err));
+router.get('/', (req, res, next) => {
+  Post.find()
+    .then(allThePosts => res.render('posts/index', { allThePosts }))
+    .catch(err => console.log(err));
 });
-
-//GET '/post'
-router.get('/post');
 
 
 //GET 'posts/new'
-router.get("/new", function(req, res, next) {
+router.get('/new', function(req, res, next) {
   res.render('posts/new');
 });
 
 //Post '/posts/new'
-router.post("/new",parser.single("image"), function(req, res, next) {
-  console.log("req.body", req.body); 
-  const { title, description,imageUrl } = req.body;
-  const image = {};
-  image.url = req.file.url;
-  image.id = req.file.public_id;
-  console.log("image", image.url);
-  
-  const newPost = new Post({ title, description,imageUrl }); 
+router.post('/new', parser.single('image'), function(req, res, next) {
+  console.log('req.body', req.body);
+  const { title, description } = req.body;
+
+  const imageUrl = req.file.secure_url;
+  console.log('image', imageUrl);
+
+  const newPost = new Post({ title, description, imageUrl });
   newPost
     .save()
     .then(book => res.redirect('/posts'))
